73c05be845f179ba377ac1fc53aad8ce9a18b2e6
kugakuen <89183294+kugakuen@users.noreply.github.com>
2021-08-19 15:51:44 +0900

    kugakuen.com test page

---
+++ script/script.js
@@ -0,0 +1,170 @@
+$(function () {
+
+    // aimation呼び出し
+    if ($('.js-scroll-trigger').length) {
+        scrollAnimation();
+    }
+
+    // aimation関数
+    function scrollAnimation() {
+        $(window).scroll(function () {
+            $(".js-scroll-trigger").each(function () {
+                let position = $(this).offset().top,
+                    scroll = $(window).scrollTop(),
+                    windowHeight = $(window).height();
+
+                if (scroll > position - windowHeight + 100) {
+                    $(this).addClass('is-active');
+                }
+            });
+        });
+    }
+    $(window).trigger('scroll');
+
+});
+
+
+
+// イベントと関数を紐付け
+document.addEventListener('touchmove', disableScroll, { passive: false });
+
+
+(function ($) {
+    var $nav = $('#navArea');
+    var $btn = $('.toggle_btn');
+    var $mask = $('#mask');
+    var $scroll = $('.scroll_ctrl');
+    var $header_list = $('.header_list');
+    var open = 'open'; // class
+    var scroll_lock = 'scroll_lock'; //class
+    // menu open close
+    $btn.on('click', function () {
+        if (!$nav.hasClass(open)) {
+            $nav.addClass(open);
+            $scroll.addClass(scroll_lock);
+            document.addEventListener('touchmove', disableScroll, { passive: false });
+        } else {
+            $nav.removeClass(open);
+            $scroll.removeClass(scroll_lock);
+            document.removeEventListener('touchmove', disableScroll, { passive: false });
+        }
+    });
+    $header_list.on('click', function () {
+        $nav.removeClass(open);
+        $scroll.removeClass(scroll_lock);
+        document.removeEventListener('touchmove', disableScroll, { passive: false });
+    });
+    // mask close
+    $mask.on('click', function () {
+        $nav.removeClass(open);
+        $scroll.removeClass(scroll_lock);
+        document.removeEventListener('touchmove', disableScroll, { passive: false });
+    });
+})(jQuery);
+
+function disableScroll(event) {
+    event.preventDefault();
+}
+
+
+$(function () {
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 5,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        responsive: [
+            {
+                breakpoint: 1190,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3,
+                    autoplay: true,
+                    autoplaySpeed: 3000,
+                    speed: 1000,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1,
+                    autoplay: true,
+                    autoplaySpeed: 3000,
+                    speed: 1000,
+                }
+            }
+        ]
+    });
+});
+
+var vGoTop = {};
+function goTop() {
+
+    vGoTop["coef"] = 50;  // ←滑らか係数（大きいほど滑らか）
+    vGoTop["cnt"] = 0;
+
+    // --- 現在のスクロール位置取得 -----
+    var startX = document.body.scrollLeft || document.documentElement.scrollLeft;
+    var startY = document.body.scrollTop || document.documentElement.scrollTop;
+
+    // --- スクロールの単位計算 ---------
+    var moveSplitCnt = 0;
+    for (var i = 1; i <= vGoTop["coef"]; i++) {
+        moveSplitCnt += i * i;
+    }
+    vGoTop["unitH"] = startY / (moveSplitCnt * 2);
+
+    vGoTop["nextX"] = startX;
+    vGoTop["nextY"] = startY;
+
+    // --- スクロール開始 ---------------
+    goTopLoop();
+}
+function goTopLoop() {
+    // ============================================================================
+    //  スクロール実行
+    // ============================================================================
+
+    vGoTop["cnt"]++;
+
+    // --- 次のスクロール位置計算 -------
+    var Coef = 0;
+    if (vGoTop["cnt"] <= vGoTop["coef"]) {
+        Coef = vGoTop["cnt"];
+    } else {
+        Coef = ((vGoTop["coef"] * 2) + 1) - vGoTop["cnt"];
+    }
+    vGoTop["nextY"] = vGoTop["nextY"] - Math.round(vGoTop["unitH"] * (Coef * Coef));
+    if ((vGoTop["cnt"] >= (vGoTop["coef"] * 2)) || (vGoTop["nextY"] <= 0)) {
+        vGoTop["nextY"] = 0;
+    }
+
+    // --- スクロール実行 ---------------
+    window.scrollTo(vGoTop["nextX"], vGoTop["nextY"]);
+
+    // --- 次のスクロールを設定 ---------
+    if (vGoTop["nextY"] <= 0) {
+        clearTimeout(vGoTop["timer"]);                   // 終了：タイマクリア
+    } else {
+        vGoTop["timer"] = setTimeout("goTopLoop()", 10);  // 次のループ
+    }
+}
+window.addEventListener("scroll", goTopDisp, false);
+function goTopDisp() {
+    // ============================================================================
+    //  先頭表示時のボタン消去
+    // ============================================================================
+    var btn = document.getElementById("topScroll");
+
+    var nowY = document.body.scrollTop || document.documentElement.scrollTop;
+    if (nowY == 0) {
+        btn.style.display = "none";
+    } else {
+        btn.style.display = "";
+    }
+}
