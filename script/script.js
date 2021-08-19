5bbb0d7a1a8b9f3140a421ef4dfcc91f357c9b56
kugakuen <89183294+kugakuen@users.noreply.github.com>
2021-08-19 16:01:40 +0900

    kugakuen.com test page

--- script/script.js
+++ script/script.js
@@ -25,46 +25,29 @@ $(function () {
 
 
 
-// イベントと関数を紐付け
-document.addEventListener('touchmove', disableScroll, { passive: false });
-
-
 (function ($) {
     var $nav = $('#navArea');
     var $btn = $('.toggle_btn');
     var $mask = $('#mask');
-    var $scroll = $('.scroll_ctrl');
     var $header_list = $('.header_list');
     var open = 'open'; // class
-    var scroll_lock = 'scroll_lock'; //class
     // menu open close
     $btn.on('click', function () {
         if (!$nav.hasClass(open)) {
             $nav.addClass(open);
-            $scroll.addClass(scroll_lock);
-            document.addEventListener('touchmove', disableScroll, { passive: false });
         } else {
             $nav.removeClass(open);
-            $scroll.removeClass(scroll_lock);
-            document.removeEventListener('touchmove', disableScroll, { passive: false });
         }
     });
     $header_list.on('click', function () {
         $nav.removeClass(open);
-        $scroll.removeClass(scroll_lock);
-        document.removeEventListener('touchmove', disableScroll, { passive: false });
     });
     // mask close
     $mask.on('click', function () {
         $nav.removeClass(open);
-        $scroll.removeClass(scroll_lock);
-        document.removeEventListener('touchmove', disableScroll, { passive: false });
     });
 })(jQuery);
 
-function disableScroll(event) {
-    event.preventDefault();
-}
 
 
 $(function () {
